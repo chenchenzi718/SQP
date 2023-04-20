d0eea382eabfef80f7a3471bbcea972e0b057884
chenchenzi718 <czh15967722357@gmail.com>
2023-04-20 21:58:59 +0800

    the first commit

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+# define a function named Rosenbrock
+def R
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("you have done this job!")
